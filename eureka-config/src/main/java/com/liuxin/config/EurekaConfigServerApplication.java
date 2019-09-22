37388b124f5aa9b23b4a667c38a583ee4de990be
Br_Liu <br_liu@Br-LiudeMBP.lan>
2019-09-22 23:00:11 +0800

    新增config对eureka进行动态配置，完善中

---
+++ eureka-config/src/main/java/com/liuxin/config/EurekaConfigServerApplication.java
@@ -0,0 +1,13 @@
+package com.liuxin.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class EurekaConfigServerApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EurekaConfigServerApplication.class, args);
+    }
+}
