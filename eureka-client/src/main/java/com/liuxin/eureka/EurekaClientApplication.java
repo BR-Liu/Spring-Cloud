e0c9a9c2cac33e60e33f073aa6ff8f4ad1e6110e
Br_Liu <br_liu@Br-LiudeMBP.lan>
2019-09-22 18:57:20 +0800

    上传客户端基础配置及启动类

---
+++ eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
@@ -0,0 +1,14 @@
+package com.liuxin.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+@SpringBootApplication
+@EnableEurekaClient
+public class EurekaClientApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(EnableEurekaClient.class, args);
+    }
+}
