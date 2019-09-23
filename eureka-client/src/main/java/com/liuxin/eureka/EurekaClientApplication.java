526d4da8def38b2066a8cdc59e017893dbdfc86e
Br_Liu <br_liu@Br-LiudeMacBook-Pro.local>
2019-09-23 10:35:53 +0800

    提交修订

--- eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
@@ -10,6 +10,5 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class EurekaClientApplication {
     public static void main(String[] args) {
         SpringApplication.run(EurekaClientApplication.class, args);
-
     }
 }
