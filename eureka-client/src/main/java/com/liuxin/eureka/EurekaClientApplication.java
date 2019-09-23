7763417cc62e4b3393f0bf6a06646372bea85259
Br_Liu <br_liu@Br-LiudeMacBook-Pro.local>
2019-09-23 10:22:29 +0800

    提交修订

--- eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
@@ -3,12 +3,12 @@ package com.liuxin.eureka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-@EnableEurekaClient
+@EnableDiscoveryClient
 public class EurekaClientApplication {
     public static void main(String[] args) {
-        SpringApplication.run(EnableEurekaClient.class, args);
+        SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
