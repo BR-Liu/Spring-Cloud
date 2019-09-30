6df5917c965e0395626c3e2e8a3b782e854f6eac
BR-liu <liuxin999999@sohu.com>
2019-09-30 10:20:41 +0800

    commit1

--- eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
+++ eureka-client/src/main/java/com/liuxin/eureka/EurekaClientApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @EnableDiscoveryClient
 public class EurekaClientApplication {
     public static void main(String[] args) {
-        //
         SpringApplication.run(EurekaClientApplication.class, args);
     }
 }
